77744c05a1ae1912dd3fb5b842099ea86cb0febb
artc95 <arthur@dataroots.io>
2023-05-07 21:51:42 +0200

    initalize Bonds class, add to BF4PY class

---
+++ bf4py/bonds.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from datetime import date, datetime, timezone, time
+
+from .connector import BF4PyConnector
+
+
+class Bonds():
+    def __init__(self, connector: BF4PyConnector = None, default_isin=None, default_mic='XETR'):
+        self.default_isin = default_isin
+        self.default_mic = default_mic
+
+        if connector is None:
+            self.connector = BF4PyConnector()
+        else:
+            self.connector = connector
+
+    def instrument_data(self, isin: str = None, mic: str = None):
+        """
+        Returns all information about given derivative ISIN.
+
+        Parameters
+        ----------
+        isin : str
+            ISIN ov valid derivative.
+
+        Returns
+        -------
+        data : TYPE
+            Dict with information.
+
+        """
+        if isin is None:
+            isin = self.default_isin
+        assert isin is not None, 'No ISIN given'
+        if mic is None:
+            mic = self.default_mic
+        assert mic is not None, 'No mic (Exchange) given'
+
+        params = {'isin': isin,
+                  'mic': mic}
+
+        data = self.connector.data_request('master_data_bond', params)
+
+        return data
