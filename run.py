c49c2de942bd06e3c97cdd2c7e512eba0446f348
artc95 <arthur95chionh@gmail.com>
2025-02-08 22:48:20 +0100

    get lastPrice from degiro input instead of bourse frankfurt ; fix input/output file names

--- run.py
+++ run.py
@@ -22,27 +22,31 @@ def main(degiro_csv: str, output_name: str):
 
 
 def extract_csv_isin(filename: str):
-    with open(filename) as csv_file:
+    with open(f'{filename}.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        isin_list = []
+        info_list = []
         for row in csv_reader:
+            info = {}
             if row[1] == '':  # rows with ISIN also have a second not-empty Curency column
                 continue
 
-            isin = row[0][-12:]  # some values in 'ISIN' column have more than ISIN, e.g. 'OETM | AT0000A0DXC2'
-            isin_list.append(isin)
+            info['isin'] = row[0][-12:]  # some values in 'ISIN' column have more than ISIN, e.g. 'OETM | AT0000A0DXC2'
+            info['lastPrice'] = (float(row[2][2:].replace(',', '.')) if ',' in row[2][2:]
+                                 else '')
+            info_list.append(info)
 
-    logging.info(f'run.extract_csv_isin - last isin in isin_list is {isin_list[-1]}')
-    return isin_list
+    logging.info(f'run.extract_csv_isin - last isin in isin_list is {info_list[-1]}')
+    return info_list
 
 
-def list_key_specs(isin_list: list):
+def list_key_specs(info_list: list):
     bf4py = BF4Py(default_isin='XS0968913342', default_mic='XFRA')
     logging.info('run.list_key_specs - class object bf4py initialized')
     specs_list = []
-    for isin in isin_list:
+    for info in info_list:
         try:
-            data = get_bond_data(bf4py, isin)
+            data = get_bond_data(bf4py, info['isin'])
+            data = {**data, **info}
             specs = extract_key_specs(data)
             specs_list.append(specs)
         except Exception as e:
@@ -68,16 +72,6 @@ def get_bond_data(bf4py, isin: str):
         """
     if not isinstance(instrument_data, dict):  # instrument data is dict, no dict means no data, proceed to next ISIN!
         return
-
-    bid_ask_latest = bf4py.bonds.bid_ask_latest(isin=isin)
-    """
-    {'isin': 'IE00B6X95T99', 'bidLimit': 0.0, 'askLimit': 0.0, 'bidSize': 0.0, 'askSize': 0.0, 'lastPrice': 100.26, 
-    'timestampLastPrice': '2023-05-12T17:00:17+02:00', 'changeToPrevDayAbsolute': 0.0, 'changeToPrevDayInPercent': 0, 
-    'spreadAbsolute': 0, 'spreadRelative': 0, 'timestamp': '2023-05-12T17:30:00+02:00', 'nominal': True, 
-    'tradingStatus': 'CLOSED', 'instrumentStatus': 'ACTIVE', 'open': 100.26}
-    """
-    instrument_data.update(bid_ask_latest)  # add bid_ask_latest dict to instrument_data dict
-
     return instrument_data
 
 
@@ -104,4 +98,4 @@ def csv_key_specs(specs_list: list, output_name: str):
     df.to_csv(f'{output_name}.csv', index=False)
 
 
-main('Book1.csv', 'degiro_bonds')
+main('input', 'output')
